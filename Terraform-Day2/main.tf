709a740481967d34a72c6ecee258623d00593688
RajendraKharade29 <kharaderajendra1@gmail.com>
2026-07-13 12:57:06 +0530

    Update main.tf

--- Terraform-Day2/main.tf
+++ Terraform-Day2/main.tf
@@ -1,1 +1,22 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name = "TerraformDay2"
+    Env  = "Dev"
+  }
+}
