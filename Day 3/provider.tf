f62da03bf77b72a63061bbef1af475ab4b8fad13
RajendraKharade29 <kharaderajendra1@gmail.com>
2026-07-16 21:11:23 +0530

    Update provider.tf

--- Day 3/provider.tf
+++ Day 3/provider.tf
@@ -1,1 +1,23 @@
+terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = "TerraWeek-Day3"
+      Environment = "Dev"
+      ManagedBy   = "Terraform"
+      Owner       = "Rajendra Kharade"
+    }
+  }
+}
