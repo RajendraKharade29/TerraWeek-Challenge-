c39a5d29e1e5a4891804ed49feb3576817c96067
RajendraKharade29 <kharaderajendra1@gmail.com>
2026-07-16 21:13:01 +0530

    Update variables.tf

--- Day 3/variables.tf
+++ Day 3/variables.tf
@@ -1,1 +1,46 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-2"
+}
 
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "EC2 Instance Name"
+  type        = string
+  default     = "TerraWeek-EC2"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "Public Subnet CIDR"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability Zone"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "common_tags" {
+  description = "Common Tags"
+  type        = map(string)
+
+  default = {
+    Project     = "TerraWeek-Day3"
+    Environment = "Dev"
+    Owner       = "Rajendra Kharade"
+  }
+}
