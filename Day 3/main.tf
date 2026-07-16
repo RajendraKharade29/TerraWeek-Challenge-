a3f2ef3310e4320fc82f1913ac712b70866c76e2
RajendraKharade29 <kharaderajendra1@gmail.com>
2026-07-16 21:14:13 +0530

    Update main.tf

--- Day 3/main.tf
+++ Day 3/main.tf
@@ -1,1 +1,133 @@
+# -----------------------------
+# Data Source - 
+# -----------------------------
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
 
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+# -----------------------------
+# VPC
+# -----------------------------
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = merge(var.common_tags, {
+    Name = "TerraWeek-VPC"
+  })
+}
+
+# -----------------------------
+# Public Subnet
+# -----------------------------
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = merge(var.common_tags, {
+    Name = "Public-Subnet"
+  })
+}
+
+# -----------------------------
+# Internet Gateway
+# -----------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.common_tags, {
+    Name = "TerraWeek-IGW"
+  })
+}
+
+# -----------------------------
+# Route Table
+# -----------------------------
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(var.common_tags, {
+    Name = "Public-Route-Table"
+  })
+}
+
+# -----------------------------
+# Route Table Association
+# -----------------------------
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# -----------------------------
+# Security Group
+# -----------------------------
+resource "aws_security_group" "web_sg" {
+  name        = "web-security-group"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.common_tags, {
+    Name = "Web-SG"
+  })
+}
+
+# -----------------------------
+# EC2 Instance
+# -----------------------------
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  tags = merge(var.common_tags, {
+    Name = var.instance_name
+  })
+
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
