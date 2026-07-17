601a53d26685317c339aa572633c9b8e0ff769e0
RajendraKharade29 <kharaderajendra1@gmail.com>
2026-07-17 21:09:54 +0530

    Update outputs.tf

--- Day 3/outputs.tf
+++ Day 3/outputs.tf
@@ -1,49 +1,19 @@
 output "instance_id" {
-  description = "EC2 Instance ID"
+  description = "ID of the EC2 instance."
   value       = aws_instance.web.id
 }
 
 output "public_ip" {
-  description = "EC2 Public IP Address"
+  description = "Public IP of the web server."
   value       = aws_instance.web.public_ip
 }
 
-output "public_dns" {
-  description = "EC2 Public DNS"
-  value       = aws_instance.web.public_dns
-}
-
-output "private_ip" {
-  description = "EC2 Private IP Address"
-  value       = aws_instance.web.private_ip
-}
-
-output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.main.id
-}
-
-output "subnet_id" {
-  description = "Public Subnet ID"
-  value       = aws_subnet.public.id
-}
-
-output "security_group_id" {
-  description = "Security Group ID"
-  value       = aws_security_group.web_sg.id
-}
-
-output "internet_gateway_id" {
-  description = "Internet Gateway ID"
-  value       = aws_internet_gateway.igw.id
-}
-
-output "route_table_id" {
-  description = "Route Table ID"
-  value       = aws_route_table.public.id
+output "web_url" {
+  description = "Open this in your browser once the instance boots."
+  value       = "http://${aws_instance.web.public_ip}"
 }
 
 output "ami_id" {
-  description = "Amazon Linux 2023 AMI ID"
-  value       = data.aws_ami.amazon_linux.id
+  description = "The Amazon Linux 2023 AMI resolved via the data source."
+  value       = data.aws_ami.al2023.id
 }
